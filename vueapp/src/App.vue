1b4e0b8eebad25e06da09366595804163ee5167c
dionisio-rubi <rdionisio@nevada.unr.edu>
2023-12-11 16:13:12 -0800

    have home page

--- vueapp/src/App.vue
+++ vueapp/src/App.vue
@@ -1,47 +1,108 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="div">
+    <header class="header">
+      <img alt="cardhub-logo" class="logo" src="./assets/logo.svg" width="200" height="200" />
+      <h1>Welcome to CardHub!</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      </header>
+    
+    <body class="body">
+      <button class="btn host-btn">Host a Game</button>
+      <button class="btn join-btn">Join</button>
 
-  <main>
-    <TheWelcome />
-  </main>
+<!--    <HelloWorld msg="You did it!" />-->
+<!--    </header>-->
+    </body>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .host-btn {
+    background-color: darkred;
+    border: none;
+    color: black;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 20px;
+    margin: 4px 2px;
+    cursor: pointer;
   }
+
+  .join-btn {
+    background-color: darkred;
+    border: none;
+    color: black;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 20px;
+    margin: 4px 2px;
+    cursor: pointer;
+  }
+
+  .host-btn:hover {
+    background-color: grey;
+    color: black;
+  }
+
+  .join-btn:hover {
+    background-color: grey;
+    color: black;
+  }
+  
+  .div {
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    background-color: #282828;
+    height: 90vh;
+    width: 90vw;
+  }
+  
+.header {
+    background-color: #282828;
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 40px;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  }
+
+.body {
+    background-color: #282828;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+}
 
   .logo {
-    margin: 0 2rem 0 0;
+    pointer-events: none;
+    align-items: center;
+  }
+  
+  .btn {
+    background-color: darkred;
+    border: none;
+    color: black;
+    width: 200px;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    font-size: 20px;
+    margin: 4px 2px;
+    cursor: pointer;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+
 </style>
---
+++ vueapp/src/pages/join.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <img alt="cardhub-logo" class="logo" src="./assets/logo.svg" scale=".25%" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
